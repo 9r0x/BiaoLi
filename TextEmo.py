bc8c238046205435d9ec745a2a3ad1dbd2577c82
GroxNi <grox@X-org.local>
2017-05-04 06:56:20 +0800

    First commit

---
+++ TextEmo.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+def TextEmo(sentence):
+    _url = "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment"
+    headers = dict()
+    headers['Content-Type'] = 'application/json'
+    headers['Ocp-Apim-Subscription-Key'] = 'a623b7dead114a72bf06d78855fa8b16'
+
+    json = {"documents": [{"language": "en","id": "1","text": sentence}]}
+    params = None
+    data = None
+
+    response = requests.request( 'post', _url, json = json, headers = headers, params = params )
+    response = json.load(response.content)
+    return response
