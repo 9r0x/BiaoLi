8d5a7c580308db992fe7972e07e762e72a9b5a65
GroxNi <grox@X-org.local>
2017-05-05 22:17:34 +0800

    Speech to Emotion

--- SpeechText.py
+++ SpeechText.py
@@ -1,16 +1,21 @@
 import speech_recognition as sr
+from TextEmo import textemo
 
-# obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
+def speechemo():
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
 
-# recognize speech using Microsoft Bing Voice Recognition
-BING_KEY = "7c9c086e169449e5bb48e6fb5ecc52b6"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-try:
-    print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio, key=BING_KEY))
-except sr.UnknownValueError:
-    print("Microsoft Bing Voice Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+        # recognize speech using Microsoft Bing Voice Recognition
+        BING_KEY = "7c9c086e169449e5bb48e6fb5ecc52b6"  # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
+        result = r.recognize_bing(audio, key=BING_KEY)
+        try:
+            print result
+            return textemo(result)
+        except sr.UnknownValueError:
+            return "Microsoft Bing Voice Recognition could not understand audio"
+        except sr.RequestError as e:
+            return "Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e)
+
+
